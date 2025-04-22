3a8120104b7a5ad3df02799effb15d062159715e
bankmotion <harris55555555555@gmail.com>
2025-04-21 17:49:32 -0700

    UPDATE

--- scraper.ts
+++ scraper.ts
@@ -1,10 +1,40 @@
 import axios from "axios";
 import * as fs from "fs";
 import * as path from "path";
+import { Page } from "puppeteer";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
+
+// Function to extract year from different date formats
+const extractYearFromDate = (
+  date: string | null | undefined
+): number | null => {
+  if (!date) return null;
+
+  const dateParts = date.split(" ");
+
+  if (dateParts.length === 2) {
+    const year = parseInt(dateParts[1], 10);
+    return isNaN(year) ? null : year;
+  }
+
+  if (dateParts.length === 1) {
+    const year = parseInt(dateParts[0], 10);
+    return isNaN(year) ? null : year;
+  }
+
+  if (dateParts.length === 3) {
+    const year = parseInt(dateParts[2], 10);
+    return isNaN(year) ? null : year;
+  }
+
+  return null;
+};
+
+const delay = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms * 1000));
 
 const scrapeArticles = async (query: string) => {
   try {
@@ -18,45 +48,79 @@ const scrapeArticles = async (query: string) => {
     });
     const page = await browser.newPage();
 
-    const searchUrl = `https://www.sciencedirect.com/search?qs=${query}&show=100&offset=0`;
-    await page.goto(searchUrl, { waitUntil: "domcontentloaded" });
+    let currentPage = 1;
 
-    await page.waitForSelector(".result-item-container");
+    while (true) {
+      const searchUrl = `https://www.sciencedirect.com/search?qs=${query}&show=100&offset=${
+        (currentPage - 1) * 100
+      }`;
+      await page.goto(searchUrl, { waitUntil: "domcontentloaded" });
 
-    const articleLinks = await page.evaluate(() => {
-      const links = Array.from(
-        document.querySelectorAll(
-          ".result-item-container a.result-list-title-link"
-        )
-      );
-      return links.map((link) => (link as HTMLAnchorElement).href);
-    });
+      await page.waitForSelector(".result-item-container");
 
-    console.log(articleLinks);
+      const articles = await page.evaluate(() => {
+        const items = Array.from(
+          document.querySelectorAll(".result-item-container")
+        );
+        return items.map((item) => {
+          const linkElement = item.querySelector(
+            "a.result-list-title-link"
+          ) as HTMLAnchorElement;
+          const title = linkElement ? linkElement.href : null;
+          const dateElement = item.querySelector(
+            ".SubType .srctitle-date-fields>span:last-child"
+          );
+          const date = dateElement ? dateElement.textContent?.trim() : null;
+          return { title, date };
+        });
+      });
 
-    // for (let i = 0; i < articleLinks.length; i++) {
-    //   const link = articleLinks[i];
-    //   const articleDOI = link.split("/").pop();
+      if (articles.length === 0) break;
+      currentPage++;
 
-    //   if (!articleDOI) continue;
+      const filteredArticles = articles.filter((article) => {
+        const year = extractYearFromDate(article.date);
+        return year && year >= 2015 && year <= 2020;
+      });
+      console.log(filteredArticles);
 
-    //   const sciHubUrl = `https://sci-hub.st/https://doi.org/${articleDOI}`;
+      for (let i = 0; i < filteredArticles.length; i++) {
+        const link = filteredArticles[i];
+        if (!link.title) continue;
 
-    //   await page.goto(sciHubUrl, { waitUntil: "domcontentloaded" });
+        await page.goto(link.title, { waitUntil: "domcontentloaded" });
+        await delay(2);
 
-    //   const pdfLink = await page.evaluate(() => {
-    //     const embedElement = document.querySelector("embed");
-    //     return embedElement ? embedElement.getAttribute("src") : null;
-    //   });
+        const articleDOI = await page.evaluate(() => {
+          const doiLink = document.querySelector(
+            "a.anchor.doi"
+          ) as HTMLAnchorElement;
+          return doiLink ? doiLink.href.split("doi.org/")[1] : null;
+        });
 
-    //   if (pdfLink) {
-    //     console.log(`Found PDF, starting download`);
-    //     await downloadPDF(pdfLink, articleDOI);
-    //   } else {
-    //     console.log(`No PDF for DOI: ${articleDOI}`);
-    //   }
-    // }
-    return articleLinks;
+        if (!articleDOI) continue;
+
+        const sciHubUrl = `https://sci-hub.st/https://doi.org/${articleDOI}`;
+
+        await page.goto(sciHubUrl, { waitUntil: "domcontentloaded" });
+        await delay(2);
+
+        const pdfLink = await page.evaluate(() => {
+          const embedElement = document.querySelector(
+            "embed"
+          ) as HTMLEmbedElement;
+          return embedElement ? embedElement.getAttribute("src") : null;
+        });
+        console.log(pdfLink);
+
+        if (pdfLink) {
+          console.log(`Found PDF, starting download`, pdfLink);
+          await downloadPDF("https://sci-hub.st" + pdfLink, articleDOI);
+        } else {
+          console.log(`No PDF for DOI: ${articleDOI}`);
+        }
+      }
+    }
   } catch (error) {
     console.error(`Error fetching search results`, error);
   }
@@ -65,7 +129,10 @@ const scrapeArticles = async (query: string) => {
 const downloadPDF = async (pdfUrl: string, doi: string) => {
   try {
     const res = await axios.get(pdfUrl, { responseType: "stream" });
-    const filePath = path.resolve(__dirname, `./pdfs/${doi}.pdf`);
+    const filePath = path.resolve(
+      __dirname,
+      `./pdfs/${new Date().getTime()}.pdf`
+    );
     const writer = fs.createWriteStream(filePath);
 
     res.data.pipe(writer);
@@ -84,7 +151,7 @@ const downloadPDF = async (pdfUrl: string, doi: string) => {
 
 const main = async (query: string) => {
   const searchResults = await scrapeArticles(query);
-  console.log(searchResults);
+  // console.log(searchResults);
 };
 
-main("psychology");
+main("math");
